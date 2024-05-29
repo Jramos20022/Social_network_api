b56bb663d5c59821978a698635dd764ff15edf62
Justin Ramos <Jramos20022@gmail.com>
2024-05-28 22:34:09 -0400

    reaction.js added

--- Develop/models/Reaction.js
+++ Develop/models/Reaction.js
@@ -1,5 +1,33 @@
 const { Schema, Types } = require("mongoose");
+const { ObjectId } = Schema.Types;
 
-const reactionSchema = new Schema();
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: ObjectId,
+      default: () => new ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 module.exports = reactionSchema;
